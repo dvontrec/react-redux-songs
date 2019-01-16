3ef804b5cd492ee40af46cb7d9d82c18f958b815
Dvontre Coleman <dvontrec@gmail.com>
2019-01-15 19:19:28 -0500

    Add song list connect to redux store

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'React';
+import { connect } from 'react-redux';
 
 class SongList extends Component {
   render() {
+    console.log(this.props);
     return <div>Song List</div>;
   }
 }
 
-export default SongList;
+// function used to pass global state (from redux store(reducers)) as props
+const mapStateToProps = state => {
+  //  sets the props to be the object returned
+  return { songs: state.songs };
+};
+
+// runs map state to props and returns the song list
+export default connect(mapStateToProps)(SongList);
