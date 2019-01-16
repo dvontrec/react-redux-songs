8ed8318c99d337cee42028dfc013b641707312dd
Dvontre Coleman <dvontrec@gmail.com>
2019-01-15 19:07:26 -0500

    Create song list component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import SongList from './SongList';
 
 const App = () => {
-  return <div>React App is up</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/SongList.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'React';
+
+class SongList extends Component {
+  render() {
+    return <div>Song List</div>;
+  }
+}
+
+export default SongList;
