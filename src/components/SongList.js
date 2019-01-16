78b2974914881f22a54485afd3bc354c7dfe6328
Dvontre Coleman <dvontrec@gmail.com>
2019-01-15 19:55:45 -0500

    Connect select song action to song list component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'React';
 import { connect } from 'react-redux';
+// import the select song action
+import { selectSong } from '../actions';
 
 class SongList extends Component {
   // helper function to render each song from the props
@@ -8,7 +10,12 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="ui right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -27,4 +34,8 @@ const mapStateToProps = state => {
 };
 
 // runs map state to props and returns the song list
-export default connect(mapStateToProps)(SongList);
+export default connect(
+  mapStateToProps,
+  // passing action to connect automatically passes it to dispatch
+  { selectSong }
+)(SongList);
